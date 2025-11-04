e16ac7f71e7f2106b733cccc5ed0973a6b103b8a
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-11-04 00:26:31 -0500

    corriger sql

--- sql/librairie.sql
+++ sql/librairie.sql
@@ -2,30 +2,44 @@ CREATE DATABASE IF NOT EXISTS librairie CHARACTER SET utf8mb4;
 
 USE librairie;
 
-CREATE TABLE IF NOT EXISTS librairie.livres (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
-    auteur VARCHAR(255) NOT NULL,
-    annee_publication INT NOT NULL,
-    genre VARCHAR(100) NOT NULL
-);
+-- CATEGORIES
 CREATE TABLE IF NOT EXISTS librairie.categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS librairie.livres;
-
-CREATE TABLE librairie.livres (
+-- EDITEURS
+CREATE TABLE IF NOT EXISTS librairie.editeurs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
-    auteur VARCHAR(255) NOT NULL,
-    annee_publication INT NOT NULL,
-    genre VARCHAR(100) NOT NULL,
-    categorie_id INT,
-    FOREIGN KEY (categorie_id) REFERENCES categories(id) ON DELETE SET NULL
+    nom VARCHAR(100) NOT NULL
 );
 
+-- AUTEURS
+CREATE TABLE IF NOT EXISTS auteurs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL
+);
+
+-- LIVRES
+CREATE TABLE IF NOT EXISTS librairie.livres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    auteur_id INT NOT NULL,
+    annee_publication INT NOT NULL,
+    categorie_id INT NOT NULL,
+    editeur_id INT NOT NULL
+);
+
+ALTER TABLE librairie.livres
+    ADD CONSTRAINT fk_auteur FOREIGN KEY (auteur_id) REFERENCES auteurs(id) ON DELETE SET NULL;
+
+ALTER TABLE librairie.livres
+    ADD CONSTRAINT fk_categorie FOREIGN KEY (categorie_id) REFERENCES categories(id) ON DELETE SET NULL;
+
+ALTER TABLE librairie.livres
+    ADD CONSTRAINT fk_editeur FOREIGN KEY (editeur_id) REFERENCES editeurs(id) ON DELETE SET NULL;
+
+-- INSERER DES DONNÉES DE TESTS
 INSERT INTO librairie.categories (nom) VALUES
 ('Roman'),
 ('Science-Fiction'),
@@ -33,31 +47,12 @@ INSERT INTO librairie.categories (nom) VALUES
 ('Biographie'),
 ('Jeunesse');
 
-CREATE TABLE IF NOT EXISTS librairie.editeurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100) NOT NULL
-);
-
-ALTER TABLE librairie.livres
-ADD COLUMN editeur_id INT NULL,
-ADD CONSTRAINT fk_editeur FOREIGN KEY (editeur_id) REFERENCES editeurs(id) ON DELETE SET NULL;
-
-INSERT INTO librarie.editeurs (nom) VALUES 
+INSERT INTO librairie.editeurs (nom) VALUES 
 ('Gallimard'),
 ('Flammarion'),
 ('Actes Sud'),
 ('Hachette'),
 ('Albin Michel');
-
-CREATE TABLE auteurs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
-);
-
-ALTER TABLE livres
-DROP COLUMN auteur,
-ADD COLUMN auteur_id INT,
-ADD CONSTRAINT fk_auteur FOREIGN KEY (auteur_id) REFERENCES auteurs(id) ON DELETE SET NULL;
 
 INSERT INTO auteurs (nom) VALUES
 ('Victor Hugo'),
@@ -65,3 +60,6 @@ INSERT INTO auteurs (nom) VALUES
 ('Marguerite Duras'),
 ('Albert Camus'),
 ('George Orwell');
+
+INSERT INTO livres (titre, auteur_id, annee_publication, categorie_id, editeur_id) VALUES
+('Je suis un livre', 1, 1990, 1, 1);
